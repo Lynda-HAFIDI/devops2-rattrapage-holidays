3bb190a28dbea4268b0cbb62689703e219ac01a0
Lynda Hafidi <233025089+Lynda-HAFIDI@users.noreply.github.com>
2026-06-26 15:19:52 +0200

    Validation de la fonctionnalité par appel REST

---
+++ src/test/java/com/devops2/rattrapage/RattrapageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops2.rattrapage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RattrapageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
